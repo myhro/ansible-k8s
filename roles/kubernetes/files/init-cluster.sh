4ef0655331da7c5b5be60f3b11abfe2cda12e10f
Tiago Ilieve <tiago.myhro@gmail.com>
2018-06-06 16:20:27 +0200

    Remove unneeded non-root user references

--- roles/kubernetes/files/init-cluster.sh
+++ roles/kubernetes/files/init-cluster.sh
@@ -5,13 +5,12 @@ set -ex
 KUBE_HOME="${HOME}/.kube"
 KUBE_CONFIG="${KUBE_HOME}/config"
 KUBE_MASTER="${KUBE_MASTER:-master.cluster.k8s}"
-KUBE_USER="${KUBE_USER:-ubuntu}"
 
 sudo kubeadm init --apiserver-cert-extra-sans "$KUBE_MASTER" --pod-network-cidr 10.244.0.0/16
 
 mkdir -p "$KUBE_HOME"
 sudo cp /etc/kubernetes/admin.conf "$KUBE_CONFIG"
-sudo chown "$KUBE_USER:$KUBE_USER" "$KUBE_CONFIG"
+sudo chown "$USER:$USER" "$KUBE_CONFIG"
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f https://raw.githubusercontent.com/myhro/ansible-k8s/master/roles/kubernetes/files/traefik.yaml
