d07c85b717566dbdd711e7b81b2eac10f873ebf8
Tiago Ilieve <tiago.myhro@gmail.com>
2018-06-13 13:21:20 +0200

    Add default resource requests and limits

--- roles/kubernetes/files/init-cluster.sh
+++ roles/kubernetes/files/init-cluster.sh
@@ -13,4 +13,5 @@ sudo cp /etc/kubernetes/admin.conf "$KUBE_CONFIG"
 sudo chown "$USER:$USER" "$KUBE_CONFIG"
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/myhro/ansible-k8s/master/manifests/limits.yaml
 kubectl apply -f https://raw.githubusercontent.com/myhro/ansible-k8s/master/manifests/traefik.yaml
